18f3db0e89b83df4611d8aec1204353839cdc12e
YuriZem <yurizem333@gmail.com>
2025-10-16 17:31:38 -0300

    more books

--- src/app.js
+++ src/app.js
@@ -6,6 +6,10 @@ const livros = [
     { id: 1, titulo: 'O Senhor dos Anéis - A Sociedade do Anel' },
     { id: 2, titulo: 'O Senhor dos Anéis - As Duas Torres' },
     { id: 3, titulo: 'O Senhor dos Anéis - O Retorno do Rei' },
+    { id: 4, titulo: 'O Senhor dos Anéis - O Retorno do Rei' },
+    { id: 5, titulo: 'O Senhor dos Anéis - O Retorno do Rei' },
+    { id: 6, titulo: 'O Senhor dos Anéis - O Retorno do Rei' },
+    { id: 7, titulo: 'O Senhor dos Anéis - O Retorno do Rei' },
 ];
 
 app.get('/', (req, res) => {
