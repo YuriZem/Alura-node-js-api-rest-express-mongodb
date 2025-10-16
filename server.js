2bc65d72c86d149c349fd2d4200561dc17c47c78
YuriZem <yurizem333@gmail.com>
2025-10-16 17:30:54 -0300

    add list for books

--- server.js
+++ server.js
@@ -2,14 +2,6 @@
 import app from './src/app.js';
 
 const PORT = process.env.PORT || 3000;
-// const PORT = 3000;
-
-const rotas = {
-    '/': 'Curso de Node.js',
-    '/livros': 'Você está na página de livros',
-    '/autores': 'Você está na página de autores',
-    '/editora': 'Você está na página de editoras'
-};
 
 app.listen(PORT, () => {
     console.log(`Servidor escutando em http://localhost:${PORT}`);
