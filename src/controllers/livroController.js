cb4a92346b61437426dd9151d868b5f4ae9ad23e
YuriZem <yurizem333@gmail.com>
2025-10-19 18:04:54 -0300

    organização das funcoes e das rotas

--- src/controllers/livroController.js
+++ src/controllers/livroController.js
@@ -3,8 +3,13 @@ import livro from "../models/Livro.js";
 class LivroController {
 
   static async listarLivros (req, res) {
-    const listBooks = await livro.find().exec();
-    res.status(200).json(listBooks);
+    try{
+      const listBooks = await livro.find().exec();
+      res.status(200).json(listBooks);
+    }catch(err){
+      console.error('Erro ao listar livros:', err);
+      res.status(500).json({ error: "Erro ao listar livros" });
+    }
   }
 
   static async add_book (req, res){
@@ -16,6 +21,39 @@ class LivroController {
       res.status(500).json({ error: "Erro ao adicionar livro" });
     }
   }
+
+  static async updateBook (req, res){
+    try {
+      const id = req.params.id;
+      await livro.findByIdAndUpdate(id,req.body);
+      res.status(200).json({message: "Livro atualizado com sucesso"});
+    } catch (error) {
+     console.error('Erro ao atualizar livro:', error);
+     res.status(500).json({ error: "Erro ao atualizar livro" }); 
+    }
+  };
+
+  static async findBookForId (req, res){
+    try {
+      const id = req.params.id;
+      const bookfound = await livro.findById(id).exec();
+      res.status(200).json(bookfound);
+    } catch (error) {
+      console.error('Erro ao buscar livro por ID:', error);
+      res.status(500).json({ error: "Erro ao buscar livro por ID" });
+    }
+  };
+
+  static async removeBook (req, res){
+    try {
+      const id = req.params.id;
+      await livro.findByIdAndDelete(id);
+      res.status(200).json({message: "Livro removido com sucesso"});
+    } catch (error) {
+      console.error('Erro ao remover livro:', error);
+      res.status(500).json({ error: "Erro ao remover livro" });
+    }
+  };
 
 };
 
--- src/routes/livrosRoutes.js
+++ src/routes/livrosRoutes.js
@@ -5,8 +5,10 @@ import LivroController from "../controllers/livroController.js";
 const routes = express.Router();  
 
 routes.get('/livros', LivroController.listarLivros);
-
 routes.post('/add_livros', LivroController.add_book);
+routes.get('/livros/:id', LivroController.findBookForId);
+routes.put('/livros/:id', LivroController.updateBook);
+routes.delete('/livros/:id', LivroController.removeBook);
 
 
 export default routes;
